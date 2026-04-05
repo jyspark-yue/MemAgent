3e0d29d469d0c9c2f53a71cd5a4de074240de569
vgarg492 <varenya.garg@gmail.com>
2026-04-05 12:31:52 -0700

    Updated proposition extraction memory block and reductive agent to be compatible with Qdrant through docker

--- asdrp/agent/reductive_agent.py
+++ asdrp/agent/reductive_agent.py
@@ -10,34 +10,39 @@
 #   @author     Eric Vincent Fernandes
 #               - Implemented tracking for token/cost metrics
 #               - Modified code to be compatible with Gemini (GenAI)
+#   @author     Varenya Garg
+#               - Modified code to store and retrieve propositions using Qdrant vector database
 #
 # Date:
 #   Created:    July 4, 2025  (Theodore Mui)
 #   Modified:   October 5, 2025 (Eric Vincent Fernandes)
+#   Modified:   April 5, 2026 (Varenya Garg)
 #############################################################################
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 from asdrp.agent.BaseAgent import BaseAgent
-
+from llama_index.llms.openai import OpenAI
 import time
 import asyncio
 from asdrp.agent.base import AgentReply
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from asdrp.memory.proposition_extraction_memory import PropositionExtractionMemoryBlock
 
 class ReductiveAgent(BaseAgent):
     def __init__(self):
-        self.memory_block = PropositionExtractionMemoryBlock(max_propositions=50)
+        super().__init__()
+        self.memory_block = PropositionExtractionMemoryBlock(collection="agent_condensed_mem", max_propositions=50)
 
     async def achat(self, user_msg: str) -> AgentReply:
         try:
             initial_query_time = time.time()
+            user_message = ChatMessage(role=MessageRole.USER, content=user_msg)
+            await self.memory_block._aput(messages=[user_message])
 
             # Prepend known propositions to the user message if available, with explicit instruction
-            props = await self.memory_block._aget()
-            if not props:
-                return AgentReply(response_str="ERROR: NO PROPOSITIONS STORED")
+            props = await self.memory_block._aget(messages=[user_message])
 
             prompt = (
                 f"User: {user_msg}\n"
